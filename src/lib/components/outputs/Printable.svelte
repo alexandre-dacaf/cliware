c4bb70630841ff75b6fccd1f1b0aa80c8d26aac9
Alexandre Assis <alexandre.dacaf@gmail.com>
2024-10-15 23:47:23 -0300

    feat: Trying react

--- src/lib/components/outputs/Printable.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import type { SvelteComponent } from "svelte";
-
-    export let component: typeof SvelteComponent;
-
-    export let props: Record<string, any> = {};
-</script>
-
-<svelte:component this={component} {...props} />
